37e2825c8dd1a701d1b8f8d8aafc4452ef831951
Thiago92Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-09-20 16:28:27 -0300

    make routes call the controllers functions

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const NoteController = require('../controllers/note');
+const UserController = require('../controllers/user');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -7,15 +10,15 @@ router.get('/', (req, res) => {
 });
 
 // Note routes
-router.get('/note/index', () => {});
-router.get('/note/index/:noteId', () => {});
-router.post('/note/add', () => {});
-router.delete('/note/delete/:noteId', () => {});
-router.put('/note/update/:noteId', () => {});
+router.get('/note/index', NoteController.index);
+router.get('/note/index/:noteId', NoteController.show);
+router.post('/note/add', NoteController.store);
+router.delete('/note/delete/:noteId', NoteController.delete);
+router.put('/note/update/:noteId', NoteController.update);
 
 // User routes
-router.post('/user/add', () => {});
-router.delete('/user/delete/:userId', () => {});
-router.put('/user/update/:userId', () => {});
+router.post('/user/add', UserController.store);
+router.delete('/user/delete/:userId', UserController.delete);
+router.put('/user/update/:userId', UserController.update);
 
 module.exports = router;
