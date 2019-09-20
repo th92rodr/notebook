96dc5ae099377dbe9f80ab3b94d3bd0959344007
th92rodr <45070690+th92rodr@users.noreply.github.com>
2019-09-20 12:34:45 -0300

    Server: add express router

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const routes = require('./routes/index');
 
 // initialize an express app
 const app = express();
+
+app.use('/', routes);
 
 module.exports = app;
