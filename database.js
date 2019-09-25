ccab2c42181e7ac482e9fb32227309fd1f273791
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-09-24 22:22:24 -0300

    add connection to a mongo db

---
+++ database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+module.exports.connect = () => {
+  mongoose
+    .connect(process.env.DATABASE, { useNewUrlParser: true })
+    .then(() => {
+      console.log('Sucessfully connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('Failed to connect to MongoDB - ', error);
+    });
+};
