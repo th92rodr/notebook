e36a37cf0f4483223a58fdf7124f9db6ca8859ac
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-09-25 20:17:36 -0300

    make delete and update operations return data

--- services/user.js
+++ services/user.js
@@ -4,49 +4,50 @@ const jwt = require('jsonwebtoken');
 const UserModel = require('../models/user');
 
 module.exports = {
+  async createUser(name, password, email) {
+    // encrypt the user's password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    async createUser(name, password, email) {
-        // encrypt the user's password
-        const hashedPassword = await bcrypt.hash(password, 1234);
+    const createdUser = await UserModel.create({
+      name,
+      email,
+      password: hashedPassword
+    });
 
-        const createdUser = await UserModel.create({
-            name,
-            email,
-            password: hashedPassword
-        });
+    return { createdUser };
+  },
 
-        return { createdUser };
-    },
+  async deleteUser(userId) {
+    const deletedUser = await UserModel.deleteOne({ _id: userId });
 
-    async deleteUser(userId) {
-        await UserModel.deleteOne({ _id: userId });
-    },
+    return { deletedUser };
+  },
 
-    async updateUser(userId, name, password, email) {
-        const updatedUser = await UserModel.updateOne(
-            { _id: userId }, 
-            { name, email, password }
-        );
+  async updateUser(userId, name, password, email) {
+    const updatedUser = await UserModel.updateOne(
+      { _id: userId },
+      { name, email, password }
+    );
 
-        return { updatedUser };
-    },
+    return { updatedUser };
+  },
 
-    async login(email, password) {
-        // get the user data in the database
-        const user = await UserModel.findOne({ email: email });
-        if (!user) throw new Error('User does not exists');
+  async login(email, password) {
+    // get the user data in the database
+    const user = await UserModel.findOne({ email: email });
+    console.log('user - ', user);
+    if (!user) throw new Error('User does not exists');
 
-        // verify if the informed password is valid
-        const isValid = await bcrypt.compare(password, user.password);
-        if (!isValid) throw new Error('Credentials are invalid');
+    // verify if the informed password is valid
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) throw new Error('Credentials are invalid');
 
-        const token = await jwt.sign(
-            { userId: user.id, email: user.email },
-            process.env.SALT,
-            { expiresIn: '1h' }
-        );
+    const token = await jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.SALT,
+      { expiresIn: '1h' }
+    );
 
-        return { token, tokenExpiration: 1 };
-    },
-
+    return { token, tokenExpiration: 1 };
+  }
 };
