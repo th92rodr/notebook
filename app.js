1857646d55bb99424abc93d9d5461b656728dc3e
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-09-25 20:06:19 -0300

    make bodyparser format json format requests

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const routes = require('./routes/index');
 
 // initialize an express app
 const app = express();
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
 app.use('/', routes);
 
