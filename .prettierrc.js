659b884f40223719e3fb731dee764746850c55ec
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-09-24 22:28:39 -0300

    add prettier dependency and config file

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * Prettier configuration file
+ */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  trailingComma: 'none'
+};
