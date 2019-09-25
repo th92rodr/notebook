6e09ee3fb5a27a7bd4152eff7f5c8eaedcd192a9
th92rodr <45070690+th92rodr@users.noreply.github.com>
2019-09-25 20:17:36 -0300

    Server: make delete and update operations return data

--- services/note.js
+++ services/note.js
@@ -1,38 +1,43 @@
 const NoteModel = require('../models/note');
+const UserModel = require('../models/user');
 
 module.exports = {
+  async getAllNotes(userId) {
+    const user = await UserModel.findOne({ id: userId });
+    const notes = await NoteModel.find({ creator: user });
+    return { notes };
+  },
 
-    async getAllNotes(user) {
-        const notes = await NoteModel.find({ creator: user });
-        return { notes };
-    },
+  async getSingleNote(noteId) {
+    const note = await NoteModel.findById(noteId);
+    return { note };
+  },
 
-    async getSingleNote(noteId) {
-        const note = await NoteModel.findById(noteId);
-        return { note };
-    },
+  async createNote(title, description, userId) {
+    const user = await UserModel.findOne({ id: userId });
+    const createdNote = await NoteModel.create({
+      title,
+      description,
+      creator: user
+    });
 
-    async createNote(title, description, user) {
-        const createdNote = await NoteModel.create({
-            title,
-            description,
-            creator: user
-        });
+    return { createdNote };
+  },
 
-        return { createdNote };
-    },
+  async deleteNote(noteId) {
+    //NoteModel.findByIdAndRemove()
+    const deletedNote = await NoteModel.deleteOne({ _id: noteId });
 
-    async deleteNote(noteId) {
-        await NoteModel.deleteOne({ _id: noteId });
-    },
+    return { deletedNote };
+  },
 
-    async updateNote(noteId, title, description) {
-        const updatedNote = await NoteModel.updateOne(
-            { _id: noteId }, 
-            { title, description }
-        );
+  async updateNote(noteId, title, description) {
+    //NoteModel.findByIdAndUpdate()
+    const updatedNote = await NoteModel.updateOne(
+      { _id: noteId },
+      { title, description }
+    );
 
-        return { updatedNote };
-    },
-
+    return { updatedNote };
+  }
 };
