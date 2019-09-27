9bc6973d5568f599a1e36ad90a6fd95f2572723f
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-09-26 22:56:49 -0300

    add validate request function

--- controllers/note.js
+++ controllers/note.js
@@ -1,51 +1,87 @@
+const { check, validationResult } = require('express-validator');
 const NoteService = require('../services/note');
 
 module.exports = {
+  async index(req, res) {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
 
-    async index(req, res) {
-        // get the current user id
-        const { userId } = req.headers;
-    
-        // get all notes from this user
-        const notes = await NoteService.getAllNotes(userId);
-    
-        // return a response with the notes
-        return res.json({ notes });
-    },
+    // get the current user id
+    const { userId } = req.headers;
 
-    async show(req, res) {
-        // get the required note id
-        const { noteId } = req.params;
+    // get all notes from this user
+    const notes = await NoteService.getAllNotes(userId);
 
-        const note = await NoteService.getSingleNote(noteId);
+    // return a response with the notes
+    return res.json({ notes });
+  },
 
-        return res.json({ note });
-    },
+  async show(req, res) {
+    // get the required note id
+    const { noteId } = req.params;
 
-    async store(req, res) {
-        const { userId } = req.headers;
-        const { title, description } = req.body;
+    const note = await NoteService.getSingleNote(noteId);
 
-        const note = await NoteService.createNote(title, description, userId);
+    return res.json({ note });
+  },
 
-        return res.json({ note });
-    },
+  async store(req, res) {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
 
-    async delete(req, res) {
-        const { noteId } = req.params;
+    const { userId } = req.headers;
+    const { title, description } = req.body;
 
-        const note = await NoteService.deleteNote(noteId);
+    const note = await NoteService.createNote(title, description, userId);
 
-        return res.json({ note });
-    },
+    return res.json({ note });
+  },
 
-    async update(req, res) {
-        const { noteId } = req.params;
-        const { title, description } = req.body;
+  async delete(req, res) {
+    const { noteId } = req.params;
 
-        const note = await NoteService.updateNote(noteId, title, description);
+    const note = await NoteService.deleteNote(noteId);
 
-        return res.json({ note });
-    },
+    return res.json({ note });
+  },
 
+  async update(req, res) {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
+
+    const { noteId } = req.params;
+    const { title, description } = req.body;
+
+    const note = await NoteService.updateNote(noteId, title, description);
+
+    return res.json({ note });
+  },
+
+  validate: function validate(method) {
+    switch (method) {
+      case 'index':
+        return [check('userId', 'UserId does not exists').exists()];
+      case 'store':
+        return [
+          check('userId', 'UserId does not exists').exists(),
+          check(
+            'description',
+            'Description is invalid or does not exists'
+          ).exists()
+        ];
+      case 'update':
+        return [
+          check(
+            'description',
+            'Description is invalid or does not exists'
+          ).exists()
+        ];
+    }
+  }
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,38 +1,96 @@
+const { check, validationResult } = require('express-validator');
 const UserService = require('../services/user');
 
 module.exports = {
+  async store(req, res) {
+    console.log('User Store');
 
-    async store(req, res) {
-        const { name, password, email } = req.body;
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
 
-        const user = await UserService.createUser(name, password, email);
+    const { name, password, email } = req.body;
+    console.log('body - ', name, password, email);
 
-        return res.json({ user });
-    },
+    const user = await UserService.createUser(name, password, email);
+    console.log('user - ', user);
 
-    async delete(req, res) {
-        const { userId } = req.params;
+    return res.status(200).json({ user });
+  },
 
-        const user = await UserService.deleteUser(userId);
+  async delete(req, res) {
+    console.log('User Delete');
 
-        return res.json({ user });
-    },
+    const { userId } = req.params;
+    console.log('params - ', userId);
 
-    async update(req, res) {
-        const { userId } = req.params;
-        const { name, password, email } = req.body;
+    const user = await UserService.deleteUser(userId);
+    console.log('user - ', user);
 
-        const user = await UserService.updateUser(userId, name, password, email);
+    return res.json({ user });
+  },
 
-        return res.json({ user });
-    },
+  async update(req, res) {
+    console.log('User Update');
 
-    async login(req, res) {
-        const { password, email } = req.body;
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
 
-        const { token, tokenExpiration } = await UserService.login(email, password);
+    const { userId } = req.params;
+    console.log('params - ', userId);
 
-        return res.json({ token, tokenExpiration });
-    },
+    const { name, password, email } = req.body;
+    console.log('body - ', name, password, email);
 
+    const user = await UserService.updateUser(userId, name, password, email);
+    console.log('user - ', user);
+
+    return res.json({ user });
+  },
+
+  async login(req, res) {
+    console.log('User Login');
+
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({ errors: errors.array() });
+
+    const { password, email } = req.body;
+    console.log('body - ', password, email);
+
+    const { token, tokenExpiration } = await UserService.login(email, password);
+    console.log('token - ', token);
+    console.log('token expiration - ', tokenExpiration);
+
+    return res.json({ token, tokenExpiration });
+  },
+
+  validate: function validate(method) {
+    switch (method) {
+      case 'store':
+      case 'update':
+        return [
+          check('name', 'Name does not exists').exists(),
+          check('email', 'Email is invalid or does not exists')
+            .exists()
+            .isEmail(),
+          check('password', 'Password is invalid or does not exists')
+            .exists()
+            .isLength({ min: 8 })
+        ];
+      case 'login':
+        return [
+          check('email', 'Email is invalid or does not exists')
+            .exists()
+            .isEmail(),
+          check('password', 'Password is invalid or does not exists')
+            .exists()
+            .isLength({ min: 8 })
+        ];
+    }
+  }
 };
