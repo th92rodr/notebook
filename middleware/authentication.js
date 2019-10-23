71a1ee559835e59d8ad3a511a4c3fe5aa6c77d35
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-10-22 22:08:31 -0300

    properly deconstruct authentication header array

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,33 +1,41 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-function isNotAuthenticated() {
-    req.isAuthenticated = false;
-    return next();
+function isNotAuthenticated(req, next) {
+  req.isAuthenticated = false;
+  return next();
 }
 
 module.exports = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) return isNotAuthenticated();
+  const authHeader = req.headers.authorization;
+  console.log('authorization ', authHeader);
+  if (!authHeader) return isNotAuthenticated(req, next);
 
-    // Authentication Header = Bearer 'token'
-    const parts = authHeader.split(' ');
-    if (!parts.length === 2) return isNotAuthenticated();
+  // Authentication Header = Bearer 'token'
+  const parts = authHeader.split(' ');
+  console.log('parts ', parts);
+  if (!parts.length === 2) return isNotAuthenticated(req, next);
 
-    const { scheme, token } = parts;
-    if (!/^Bearer$/i.test(scheme)) return isNotAuthenticated();
-    if (!token || token === '' ) return isNotAuthenticated();
+  const [ scheme, token ] = parts;
+  console.log('token ', token);
+  console.log('scheme ', scheme);
+  if (!/^Bearer$/i.test(scheme)) return isNotAuthenticated(req, next);
+  if (!token || token === '') return isNotAuthenticated(req, next);
 
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, process.env.SALT);
-    } catch (error) {
-        return isNotAuthenticated();
-    }
+  console.log('token ', token);
 
-    if (!decodedToken) return isNotAuthenticated();
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, process.env.SALT);
+  } catch (error) {
+    console.log('error ', error);
+    return isNotAuthenticated(req, next);
+  }
+  console.log('decodedToken ', decodedToken);
 
-    req.isAuthenticated = true;
-    req.userId = decodedToken.userId;
-    return next();
+  if (!decodedToken) return isNotAuthenticated(req, next);
+
+  req.isAuthenticated = true;
+  req.userId = decodedToken.userId;
+  return next();
 };
