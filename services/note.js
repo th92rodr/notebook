a5f8ddbaf2359c06172e0ec4d2f42339f692b040
th92rodr <45070690+th92rodr@users.noreply.github.com>
2019-10-22 21:58:05 -0300

    Server: catch errors in note service methods

--- services/note.js
+++ services/note.js
@@ -3,41 +3,66 @@ const UserModel = require('../models/user');
 
 module.exports = {
   async getAllNotes(userId) {
-    const user = await UserModel.findOne({ id: userId });
-    const notes = await NoteModel.find({ creator: user });
-    return { notes };
+    try {
+      const user = await UserModel.findOne({ id: userId });
+      if (!user) throw new Error('User does not exists');
+
+      const notes = await NoteModel.find({ creator: user });
+
+      return { notes };
+    } catch (error) {
+      throw error;
+    }
   },
 
   async getSingleNote(noteId) {
-    const note = await NoteModel.findById(noteId);
-    return { note };
+    try {
+      const note = await NoteModel.findById(noteId);
+      return { note };
+    } catch (error) {
+      throw error;
+    }
   },
 
   async createNote(title, description, userId) {
-    const user = await UserModel.findOne({ id: userId });
-    const createdNote = await NoteModel.create({
-      title,
-      description,
-      creator: user
-    });
+    try {
+      const user = await UserModel.findOne({ id: userId });
+      if (!user) throw new Error('User does not exists');
 
-    return { createdNote };
+      const createdNote = await NoteModel.create({
+        title,
+        description,
+        creator: user
+      });
+
+      return { createdNote };
+    } catch (error) {
+      throw error;
+    }
   },
 
   async deleteNote(noteId) {
-    //NoteModel.findByIdAndRemove()
-    const deletedNote = await NoteModel.deleteOne({ _id: noteId });
+    try {
+      //NoteModel.findByIdAndRemove()
+      const deletedNote = await NoteModel.deleteOne({ _id: noteId });
 
-    return { deletedNote };
+      return { deletedNote };
+    } catch (error) {
+      throw error;
+    }
   },
 
   async updateNote(noteId, title, description) {
-    //NoteModel.findByIdAndUpdate()
-    const updatedNote = await NoteModel.updateOne(
-      { _id: noteId },
-      { title, description }
-    );
+    try {
+      //NoteModel.findByIdAndUpdate()
+      const updatedNote = await NoteModel.updateOne(
+        { _id: noteId },
+        { title, description }
+      );
 
-    return { updatedNote };
+      return { updatedNote };
+    } catch (error) {
+      throw error;
+    }
   }
 };
