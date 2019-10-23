07c769879d142ff0a37451fe8a998d439ca4e313
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-10-22 21:52:55 -0300

    catch errors in note controller methods

--- controllers/note.js
+++ controllers/note.js
@@ -3,64 +3,84 @@ const NoteService = require('../services/note');
 
 module.exports = {
   async index(req, res) {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty())
-      return res.status(422).json({ errors: errors.array() });
+    try {
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(422).json({ errors: errors.array() });
 
-    // get the current user id
-    const { userId } = req.headers;
+      // get the current user id
+      const { userId } = req.headers;
 
-    // get all notes from this user
-    const notes = await NoteService.getAllNotes(userId);
+      // get all notes from this user
+      const notes = await NoteService.getAllNotes(userId);
 
-    // return a response with the notes
-    return res.status(200).json({ notes });
+      // return a response with the notes
+      return res.status(200).json({ notes });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
   },
 
   async show(req, res) {
-    // get the required note id
-    const { noteId } = req.params;
+    try {
+      // get the required note id
+      const { noteId } = req.params;
 
-    const note = await NoteService.getSingleNote(noteId);
+      const note = await NoteService.getSingleNote(noteId);
 
-    return res.status(200).json({ note });
+      return res.status(200).json({ note });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
   },
 
   async store(req, res) {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty())
-      return res.status(422).json({ errors: errors.array() });
+    try {
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(422).json({ errors: errors.array() });
 
-    const { userId } = req.headers;
-    const { title, description } = req.body;
+      const { userId } = req.headers;
+      const { title, description } = req.body;
 
-    const note = await NoteService.createNote(title, description, userId);
+      const note = await NoteService.createNote(title, description, userId);
 
-    return res.status(200).json({ note });
+      return res.status(200).json({ note });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
   },
 
   async delete(req, res) {
-    const { noteId } = req.params;
+    try {
+      const { noteId } = req.params;
 
-    const note = await NoteService.deleteNote(noteId);
+      const note = await NoteService.deleteNote(noteId);
 
-    return res.status(200).json({ note });
+      return res.status(204).end();
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
   },
 
   async update(req, res) {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty())
-      return res.status(422).json({ errors: errors.array() });
+    try {
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(422).json({ errors: errors.array() });
 
-    const { noteId } = req.params;
-    const { title, description } = req.body;
+      const { noteId } = req.params;
+      const { title, description } = req.body;
 
-    const note = await NoteService.updateNote(noteId, title, description);
+      const note = await NoteService.updateNote(noteId, title, description);
 
-    return res.status(200).json({ note });
+      return res.status(204).end();
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
   },
 
   validate: function validate(method) {
