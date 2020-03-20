411de137a5d4ce2f086b8f3998d0d6971d5d0053
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-03-20 17:21:02 -0300

    add bin/www

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-
-const PORT = 5000;
-const HOST = '0.0.0.0';
-
-const server = app.listen(PORT, HOST, () => {
-    console.log(`Server is running on port ${server.address().port}`);
-});
