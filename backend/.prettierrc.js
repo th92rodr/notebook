a44a4d793caaa879fe95d8c65c2261a8a724576d
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-03-23 19:27:23 -0300

    update prettier and eslint config

--- backend/.prettierrc.js
+++ backend/.prettierrc.js
@@ -1,6 +1,3 @@
-/**
- * Prettier configuration file
- */
 module.exports = {
   printWidth: 80,
   tabWidth: 2,
@@ -8,6 +5,7 @@ module.exports = {
   semi: true,
   singleQuote: true,
   bracketSpacing: true,
-  arrowParens: 'always',
-  trailingComma: 'none'
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  endOfLine: 'lf'
 };
