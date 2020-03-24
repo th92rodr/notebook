c9ebed72c0fa1b623d778258e131df5ced41badf
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-03-24 18:21:01 -0300

    Server: change redis docker services names

--- backend/src/redis/cache.js
+++ backend/src/redis/cache.js
@@ -3,10 +3,10 @@ const redis = require('redis');
 const redisClient = redis
   .createClient(process.env.CACHE_REDIS_URL)
   .on('connect', () => {
-    console.log('Redis client connected');
+    console.log('Redis Cache client connected');
   })
   .on('error', error => {
-    console.log('Redis not connected', error);
+    console.log('Redis Cache not connected', error);
   });
 
 module.exports = redisClient;
