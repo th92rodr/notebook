ebdabf606ab43d4a72358929378846fc1a831a39
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-03-24 18:21:01 -0300

    change redis docker services names

--- backend/src/redis/tokenControl.js
+++ backend/src/redis/tokenControl.js
@@ -3,10 +3,10 @@ const redis = require('redis');
 const redisClient = redis
   .createClient(process.env.AUTH_REDIS_URL)
   .on('connect', () => {
-    console.log('Redis client connected');
+    console.log('Redis Auth Control client connected');
   })
   .on('error', error => {
-    console.log('Redis not connected', error);
+    console.log('Redis Auth Control not connected', error);
   });
 
 module.exports = redisClient;
