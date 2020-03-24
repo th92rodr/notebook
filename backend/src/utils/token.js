94535bb862f03c772a3a6cb2fef6752bfe2f0140
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-03-23 23:12:26 -0300

    centralize token check validation

---
+++ backend/src/utils/token.js
@@ -0,0 +1,29 @@
+async function checkTokenValidation(user, token) {
+  try {
+    const data = await getAsync(authControlRedis, user);
+
+    if (token !== data.token) {
+      throw Error;
+    }
+
+    const currentDate = new Date();
+    const expirationDate = new Date(data.expiration);
+
+    if (currentDate > expirationDate) {
+      throw Error;
+    }
+  } catch (error) {
+    return {
+      isValid: false,
+      errorMessage: 'Invalid token'
+    };
+  }
+
+  return {
+    isValid: true
+  };
+}
+
+module.exports = {
+  checkTokenValidation
+};
