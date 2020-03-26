ac30a4cc6606f51ff5a67769fa16d45aab681fe6
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-03-25 22:57:40 -0300

    update logging data and create a log file

--- backend/src/middlewares/logger.js
+++ backend/src/middlewares/logger.js
@@ -1,23 +1,32 @@
 const morgan = require('morgan');
+const fs = require('fs');
 
-const logger = morgan((tokens, req, res) => {
-  return [
-    '[',
-    tokens.method(req, res),
-    ']',
-    '"',
-    tokens.url(req, res),
-    '"',
-    tokens.date(req, res, 'web'),
-
-    'STATUS:',
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'),
-    '-',
-    'response time:',
-    tokens['response-time'](req, res),
-    'ms'
-  ].join(' ');
+// create a write stream (in append mode)
+const logFileStream = fs.createWriteStream('/var/log/app.log', {
+  flags: 'a'
 });
+
+const logger = morgan(
+  (tokens, req, res) => {
+    return [
+      tokens['status'](req, res),
+      ' [',
+      tokens['method'](req, res),
+      ']\t',
+      tokens['url'](req, res),
+      '\tHTTP/',
+      tokens['http-version'](req, res),
+      '\t| response time: ',
+      tokens['response-time'](req, res),
+      ' ms\t| FROM: ',
+      tokens['user-agent'](req, res),
+      ' - ',
+      tokens['remote-addr'](req, res),
+      '\t| DATE: ',
+      tokens['date'](req, res, 'web')
+    ].join('');
+  },
+  { stream: logFileStream }
+);
 
 module.exports.logger = logger;
