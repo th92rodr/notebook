d0eb7f4f98023e6b4b85c891bc469993af3cdbf6
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-03-25 23:19:36 -0300

    Web: store the API routes in constants

---
+++ frontend/src/common/constants.js
@@ -0,0 +1,39 @@
+export default {
+  LOGIN: {
+    route: '/users/login/',
+    method: 'POST'
+  },
+  CREATE_USER: {
+    route: '/users/',
+    method: 'POST'
+  },
+  DELETE_USER: {
+    route: '/users/',
+    method: 'DELETE'
+  },
+  UPDATE_USER: {
+    route: '/users/',
+    method: 'PUT'
+  },
+
+  CREATE_NOTE: {
+    route: '/notes/',
+    method: 'POST'
+  },
+  DELTE_NOTE: {
+    route: '/notes/',
+    method: 'DELETE'
+  },
+  UPDATE_NOTE: {
+    route: '/notes/',
+    method: 'PUT'
+  },
+  GET_NOTES: {
+    route: '/notes/',
+    method: 'GET'
+  },
+  GET_SINGLE_NOTE: {
+    route: '/notes/',
+    method: 'GET'
+  }
+};
