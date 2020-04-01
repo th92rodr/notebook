f4a217d66fab4b15497d7ffe28b485263fe1e20c
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-03-31 21:07:58 -0300

    Server: start test db connection before all tests

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -1,11 +1,12 @@
 process.env.NODE_ENV = 'test';
 
+require('dotenv').config();
 require('jest');
+const request = require('supertest');
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
-const request = require('supertest');
 const app = require('../src/app');
-
 const User = require('../src/models/User');
 
 const okName = 'test name';
@@ -15,16 +16,30 @@ const badEmail = 'test';
 const badPassword = '123';
 
 beforeAll(async () => {
-  await User.collection.drop();
+  const databaseURL = process.env.TEST_DATABASE;
+  await mongoose.connect(
+    databaseURL,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    error => {
+      if (error) {
+        console.error(error);
+        process.exit(1);
+      }
+    }
+  );
 });
 
 afterAll(async () => {
-  await User.collection.drop();
+  await mongoose.connection.close();
 });
 
 describe('Create user on /users/ POST', () => {
   afterEach(async () => {
-    await User.collection.drop();
+    //await User.collection.drop();
   });
 
   test('should return status 201 and sucess message', async () => {
@@ -88,11 +103,10 @@ describe('Create user on /users/ POST', () => {
   });
 });
 
-describe('Update user on /users/ PUT', () => {});
-
 describe('Delete user on /users/ DELETE', () => {
   let userId;
   let token;
+
   beforeEach(async () => {
     await request(app)
       .post('/users/')
@@ -111,22 +125,24 @@ describe('Delete user on /users/ DELETE', () => {
     console.log('login res ', res.body);
 
     token = res.body.token;
-    userId = await jwt.decode(token).id;
+    userId = jwt.verify(token, process.env.SALT).id;
     console.log('token ', token);
     console.log('userId ', userId);
   });
 
   afterEach(async () => {
-    await User.collection.drop();
+    //await User.collection.drop();
   });
 
   test('should return status 200 and sucess message', async () => {
     const res = await request(app)
       .delete(`/users/${userId}`)
-      .set('Authorization', token);
-    console.log('Result ', res.body);
+      .set('Authorization', `Bearer ${token}`);
+    //console.log('Result ', res.body);
 
     expect(res.statusCode).toEqual(200);
     expect(res.body.message).toBe('User deleted sucessfully');
   });
 });
+
+describe('Update user on /users/ PUT', () => {});
